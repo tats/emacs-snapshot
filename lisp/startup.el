be3e084c88ddd5292f485fd59216d016fec6ba99
Glenn Morris <rgm@gnu.org>
2014-11-09 23:12:37 -0800

    Nil load-path elements shouldn't crash Emacs
    This upstream patches has been added:
    
      * lisp/startup.el (command-line): Handle nil elements in load-path.
    
    Origin: upstream, http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=edfdb22f674312389ccf5d5e37efa4d3f1516994
    Bug-Debian: http://bugs/debian.org/768751
    Added-by: Rob Browning <rlb@defaultvalue.org>

--- lisp/startup.el
+++ lisp/startup.el
@@ -1336,6 +1336,7 @@ the `--debug-init' option to view a complete error backtrace."
   (let (warned)
     (dolist (dir load-path)
       (and (not warned)
+	   (stringp dir)
 	   (string-match-p "/[._]emacs\\.d/?\\'" dir)
 	   (string-equal (file-name-as-directory (expand-file-name dir))
 			 (expand-file-name user-emacs-directory))
